0e4fb1c4ddae554e6bebc93ef72632f2a0816aff
Rob Beane <robbeane@gmail.com>
2012-10-05 13:51:33 -0400

    msm7x30: Add wireless N to wifi bands
    Change-Id: I3c1a13cfe237b42a25e417f0b4afc9c5c8560087

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,7 +54,7 @@ WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
 WIFI_DRIVER_MODULE_NAME          := "bcm4329"
 WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration iface_name=wlan"
 BOARD_WLAN_DEVICE_REV            := bcm4329
-WIFI_BAND                        := 802_11_ABG
+WIFI_BAND                        := 802_11_ABGN
 
 BOARD_USES_ADRENO_200 := true
 
